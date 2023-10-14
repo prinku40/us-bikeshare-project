2a96d75da6705df53f911d7d47046ce382c273c9
prinku40 <145813269+prinku40@users.noreply.github.com>
2023-10-13 21:07:50 -0400

    Update bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -1,5 +1,4 @@
-Python 3.11.5 (v3.11.5:cce6ba91b3, Aug 24 2023, 10:50:31) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
+
 
 
 # Function for problem 1
